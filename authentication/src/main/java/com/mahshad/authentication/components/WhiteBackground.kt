f2b46648391f0502a2ae3ceb4454e6931ad6813f
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-09-09 14:12:08 -0400

    the background for the login and signup is created

--- authentication/src/main/java/com/mahshad/authentication/components/WhiteBackground.kt
+++ authentication/src/main/java/com/mahshad/authentication/components/WhiteBackground.kt
@@ -1,6 +1,6 @@
 package com.mahshad.authentication.components
 
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,9 +15,9 @@ fun WhiteBackground(form: () -> Unit) {
     Surface(
         modifier = Modifier
             .width(430.dp)
-            .height(745.dp)
+            .fillMaxHeight()
             .padding(top = 150.dp),
-        color = Color.White,
+        color =  Color(0xFFF1FFF3),
         shape = RoundedCornerShape(
             topStart = 40.dp,
             topEnd = 40.dp,
--- authentication/src/main/java/com/mahshad/authentication/login/LogIn.kt
+++ authentication/src/main/java/com/mahshad/authentication/login/LogIn.kt
@@ -1,7 +1,13 @@
 package com.mahshad.authentication.login
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahshad.authentication.components.BlueBackground
+import com.mahshad.authentication.components.WhiteBackground
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,4 +20,14 @@ fun LoginScreen(
     Text(
         text = "Welcome to a styled text example!"
     )
+    Box(Modifier.fillMaxSize()) {
+        BlueBackground()
+        WhiteBackground({})
+    }
+}
+
+@Preview
+@Composable
+fun View() {
+    LoginScreen({})
 }
