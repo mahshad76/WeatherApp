9f06e35737267f3f4b6356740d1608d3e252d4be
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-09-24 19:12:59 -0400

    changing the icon size of the bottom bar to 24dp in the systemdesign module

--- systemdesign/src/main/java/com/mahshad/systemdesign/BottomAppBar.kt
+++ systemdesign/src/main/java/com/mahshad/systemdesign/BottomAppBar.kt
@@ -1,11 +1,14 @@
 package com.mahshad.systemdesign
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -36,7 +39,8 @@ fun BottomAppBar(
                         } else {
                             item.unselectedIcon
                         },
-                        contentDescription = item.title
+                        contentDescription = item.title,
+                        modifier = Modifier.size(24.dp)
                     )
                 },
                 label = { Text(text = item.title) }
